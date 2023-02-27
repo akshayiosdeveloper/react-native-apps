d6773a874432f89b06187f767b7166df2fd88845
Akshay Kumar <akshaychaudhary55@gmail.com>
2023-02-27 10:51:09 +0530

    added axios https utility

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,18 +1,32 @@
 import React, {useState} from "react";
 import { View,Text,StyleSheet } from "react-native";
 import SearchBar from "./components/SearchBar";
-
+import yelp from "../api/yelp";
 const SearchScreen = () => {
-  const [keyword, setKeyword] =  useState('')
+  const [keyword, setKeyword] =  useState('');
+  const [results, setResults] = useState([]);
+  const searchApi = async () => {
+   const response = await yelp.get('/search', {
+    params: {
+      limit:50,
+      term,
+      location: 'san jose'
+    }
+   });
+  setResults(response.data.businessess)
+  };
   return (
     <View style={styles.background}>
-      <SearchBar keyword={keyword} onKeywordChange={ (newKeyword) => setKeyword(newKeyword)} />
+      <SearchBar 
+      keyword={keyword} 
+      onKeywordChange={ setKeyword}
+      onTermSubmit={searchApi}
+      />
         <Text>hELLO SEARCH!!</Text>
-        <Text>{keyword}</Text>
+        <Text>We have found {results.length} results</Text>
     </View>
   );
-};
-
+}
 const styles = StyleSheet.create({
   background:{
     backgroundColor:'#FFFF00'
