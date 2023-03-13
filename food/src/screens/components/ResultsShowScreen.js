73aac531ca9a327c28e40ccb502bc024fcf44e9c
Akshay Kumar <akshay.kumar@system73.com>
2023-03-13 10:55:46 +0530

    change the message

--- food/src/screens/components/ResultsShowScreen.js
+++ food/src/screens/components/ResultsShowScreen.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { View,Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 
 const ResultShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam('id');
+  const getResult = async(id) => {
+   const response = yelp.get(`/${id}`);
+   console.log(response.data);
+   setResult(response.data);
+   
+  };
   console.log("<==========>",id);
+  useEffect(() => {
+    getResult(id);
+  }, []);
 return (
     <View>
         <Text>Results Show</Text>    
