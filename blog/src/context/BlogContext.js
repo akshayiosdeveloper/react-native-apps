00acdad3e15aa6fd00b76d165adc7ea787f137f7
Akshay Kumar <akshaychaudhary55@gmail.com>
2023-03-17 13:35:36 +0530

    Update BlogContext.js

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BlogContext = React.createContext();
-
+// use context for passing value to the nested children.
 export const BlogProvider = ({children}) => {
  return <BlogContext.Provider value={888}>
  {children}
