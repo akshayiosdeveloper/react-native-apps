573e4507e7f3e0b576543d8a7dada9309756235f
Akshay Kumar <akshay.kumar@system73.com>
2023-03-17 13:34:03 +0530

    use context for passing values.

--- blog/App.js
+++ blog/App.js
@@ -1,18 +1,21 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from "./src/screens/IndexScreen";
-
+import { BlogProvider } from "./src/context/BlogContext";
 const navigator = createStackNavigator({
   Index: IndexScreen
 
 },{
 intialRouteName: 'Index',
 defaultNavigationOptions: {
-  title:'Blogs'
+  title:'Blogs!!!'
 }
 });
 
 const App = createAppContainer(navigator);
 export default () => {
-return <App />
+return (<BlogProvider>
+  <App />
+</BlogProvider>
+);
 };
