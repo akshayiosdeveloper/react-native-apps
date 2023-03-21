a8e59ba11b80006b9254b4a5907127556258d7dc
Akshay Kumar <akshaychaudhary55@gmail.com>
2023-03-21 09:37:16 +0530

    Update BlogContext.js

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -4,6 +4,7 @@ const BlogContext = React.createContext();
 
 // use context for passing value to the nested children.
 export const BlogProvider = ({children}) => {
+    // pass the dummy objects to check functionality
     const blogPosts = [
         {title: 'Blog Post #1'},
         {title: 'Blog Post #2'}
