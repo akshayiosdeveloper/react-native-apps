54522af258c577b52ab67ff2c0efbb8a740b0dac
Akshay Kumar <akshay.kumar@system73.com>
2023-03-21 10:40:20 +0530

    add comment

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -4,16 +4,12 @@ const BlogContext = React.createContext();
 
 // use context for passing value to the nested children.
 export const BlogProvider = ({children}) => {
+    
   const [blogPosts, setBlogPosts] = useState([]);
-   // add blog post method .. The three dot is used to create new array with copying  content of old array.
+
   const addBlogPost = () => {
    setBlogPosts([...blogPosts, {title:`Blog post #${blogPosts.length + 1}`}]);
   };
-    // pass the dummy objects to check functionality
-    // const blogPosts = [
-    //     {title: 'Blog Post #1'},
-    //     {title: 'Blog Post #2'}
-    // ];
  return <BlogContext.Provider value={{data:blogPosts,addBlogPost:addBlogPost}}>
  {children}
  </BlogContext.Provider>
