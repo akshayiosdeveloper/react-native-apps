bcd50afdab758f3eaaab0bdecc715352c02d118e
Akshay Kumar <akshay.kumar@system73.com>
2023-03-21 10:14:30 +0530

    add additonal methods

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 
 const BlogContext = React.createContext();
 
 // use context for passing value to the nested children.
 export const BlogProvider = ({children}) => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+   setBlogPosts([...blogPosts, {title:`Blog post #${blogPosts.length + 1}`}]);
+  };
     // pass the dummy objects to check functionality
-    const blogPosts = [
-        {title: 'Blog Post #1'},
-        {title: 'Blog Post #2'}
-    ];
- return <BlogContext.Provider value={blogPosts}>
+    // const blogPosts = [
+    //     {title: 'Blog Post #1'},
+    //     {title: 'Blog Post #2'}
+    // ];
+ return <BlogContext.Provider value={{data:blogPosts,addBlogPost:addBlogPost}}>
  {children}
  </BlogContext.Provider>
 };
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,13 +1,14 @@
 import React,{useContext} from "react";
-import { View,Text,StyleSheet,FlatList } from "react-native";
+import { View,Text,StyleSheet,FlatList,Button } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
-    const blogPost = useContext(BlogContext);
+    const {data, addBlogPost} = useContext(BlogContext);
     return (
         <View>
        <Text>IndexScreen</Text>
-      <FlatList data={blogPost}
+       <Button title="Add Post" onPress={addBlogPost } />
+      <FlatList data={data}
        keyExtractor={(blogPost)=> blogPost.title}
        renderItem={({item}) => {
          return <Text>{item.title}</Text>
