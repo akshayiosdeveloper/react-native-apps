25381e1e0563e7e7c5d4046580f05b30982c9b1d
Akshay Kumar <akshaychaudhary55@gmail.com>
2023-03-23 10:22:20 +0530

    Update createDataContext.js

--- blog/src/context/createDataContext.js
+++ blog/src/context/createDataContext.js
@@ -2,7 +2,9 @@ import React, {useReducer} from "react";
 
 export default (reducer,actions,initialState) => {
 const Context = React.createContext();
-
+    
+// separate the provider 
+    
 const Provider = ({children}) => {
     const [state,dispatch] = useReducer(reducer,initialState);
     //actions === {addBlogPost:(dispatch) => {return () => {}}}
