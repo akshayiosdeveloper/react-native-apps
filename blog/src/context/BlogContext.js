6561b2dd883412f01580adad4ebf2fab86b2086f
Akshay Kumar <akshay.kumar@system73.com>
2023-03-24 10:26:53 +0530

    adding functionality for addition and deletion of post

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,8 +1,16 @@
 import createDataContext from "./createDataContext";
 const blogReducer = (state,action) => {
  switch(action.type) {
+   case 'delete_blogpost':
+    return state.filter((blogPost) => blogPost.id !== action.payload);
+
    case 'add_blogpost':
-  return [...state, {title:`BlogPost #${state.length + 1}`}];
+  return [...state, 
+    {
+      id:Math.floor(Math.random()*9999), 
+      title:`BlogPost #${state.length + 1}`
+    }
+  ];
   default :
   return state; 
  }
@@ -13,9 +21,15 @@ const addBlogPost = dispatch => {
   };
 };
 
+const deleteBlogPost = dispatch => {
+  return (id) => {
+    dispatch({type: 'delete_blogpost',payload:id});
+  };
+};
+
 export const { Context , Provider} = createDataContext(
   blogReducer,
-  {addBlogPost},
+  {addBlogPost,deleteBlogPost},
   []
   );
 
